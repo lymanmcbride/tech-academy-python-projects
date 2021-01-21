59d1df1ea5bb20a16bd040c2776fff21fc6459a4
Lyman McBride <68021552+lymanmcbride@users.noreply.github.com>
2021-01-20 21:27:08 -0700

    add comments and change dolphin attribute

--- learning_scripts/class_inheritance.py
+++ learning_scripts/class_inheritance.py
@@ -1,3 +1,4 @@
+#parent class: zoo animals in general
 class zoo_animal:
     def __init__(self, species, name, gender, food):
         self.species = species
@@ -5,12 +6,14 @@ class zoo_animal:
         self.gender = gender
         self.food = food
 
+#two child classes, specific animals have extra attributes,
+#but still inherit all the same attributes as the animal class
 class bear(zoo_animal):
     def __init__(self, bear_type, claw_length):
         self.bear_type = bear_type
         self.claw_length = claw_length
 
 class dolphin(zoo_animal):
-    def __init__(self, dolphin_type, enclosure):
+    def __init__(self, dolphin_type, water_type):
         self.dolphin_type = dolphin_type
-        self.enclosure = enclosure
+        self.water_type = water_type
