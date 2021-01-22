c9fd45568ec2305a1383e94bca4b7cb0927cf77e
Lyman McBride <68021552+lymanmcbride@users.noreply.github.com>
2021-01-21 20:42:29 -0700

    add more attributes to child classes

--- learning_scripts/polymorphism.py
+++ learning_scripts/polymorphism.py
@@ -10,17 +10,21 @@ class Client: #Parent Class
         print("Name: {} \nEmail: {}".format(self.name, self.email))  #get info polymorphs in all of them because it will need to display any extra info.
 
 class Company(Client):  #child class, company as sub class of client
-    def __init__(self, name, email, rep_number):
+    def __init__(self, name, email, rep_number, rep_name):
         super().__init__(name, email)   #invoking the properties of the parent class, because if you re-state __init__ it overrides the parent class
-        self.rep_number = rep_number  
+        self.rep_number = rep_number
+        self.rep_name = rep_name
+
     
     def get_info(self):
         print("Name: {} \nEmail: {}\n Representative Number: {}".format(self.name, self.email, self.rep_number)) #polymorphed get_info method
 
 class Individual(Client): #child class
-    def __init__(self, name, email, recent_purchases):
+    def __init__(self, name, email, recent_purchases, username, password):
         super().__init__(name, email)
         self.recent_purchases = recent_purchases  #could be used to give new offers since they bought something recently to incentivize more buying
+        self.username = username
+        self.password = password
     def make_purchase(self):
         self.recent_purchases = True  #polymorph of parent class method
         print("Thank you for your purchase {}!".format(self.name))
@@ -29,8 +33,8 @@ class Individual(Client): #child class
 
 
 
-jason = Individual("Jason Mraz", "jmz@singer.com", False)
-exon = Company("Exon Mobile", "em@emobile.com", "719-650-4565")
+jason = Individual("Jason Mraz", "jmz@singer.com", False, "jmz", "I L0ve mu$ic")
+exon = Company("Exon Mobile", "em@emobile.com", "719-650-4565", "Jeff Flake")
 jason.get_info()
 jason.make_purchase()
 jason.get_info()
