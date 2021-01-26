3144f655bf306fb7828a47718e4798c13dc3a892
Lyman McBride <68021552+lymanmcbride@users.noreply.github.com>
2021-01-25 21:22:15 -0700

    add comments and fix none print at the end

--- learning_scripts/encapsulation.py
+++ learning_scripts/encapsulation.py
@@ -1,18 +1,18 @@
-class using_private:
+class using_private: 
     def __init__(self):
-        self._firstName = 'Lyman'
-        self.__lastName = 'McBride'
+        self._firstName = 'Lyman' #Protected variable
+        self.__lastName = 'McBride' #private variable
 
     def getLastname(self):
-        print(self.__lastName)
+        print(self.__lastName) #accessing the private variable via method
     
     def changeLname(self, change):
-        self.__lastName = change
+        self.__lastName = change #changing the private variable using a method
 
-me = using_private()
+me = using_private() 
 me.getLastname()
-me.changeLname('Sandholtz')
+me.changeLname('Sandholtz') #using the method to change the last name
+print(me._firstName) #accessing the protected variable
+me._firstName = 'Lemon' #changing the protected variable
 print(me._firstName)
-me._firstName = 'Lemon'
-print(me._firstName)
-print(me.getLastname())
+me.getLastname()
